c6d0758350411eb952fdfd4face8e308d0bcbe37
Jeshvina <52188942+Jeshvina@users.noreply.github.com>
2019-06-27 14:32:44 +0530

    Create oi.py

---
+++ oi.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+for i in range((a+1),b):
+    flag=0
+    if(i>2):
+        for j in range(2,i):
+            if((i%j)==0):
+                flag=1
+                break
+    if(flag==0):
+        print(i)
